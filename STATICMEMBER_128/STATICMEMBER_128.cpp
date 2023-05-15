7c60d8c13a04512b4aa321b43bc696d4c217da4d
iqwa1107 <ramahs49@gmail.com>
2023-05-15 22:27:59 +0700

    membuat mahasiswa print A11

--- STATICMEMBER_128/STATICMEMBER_128.cpp
+++ STATICMEMBER_128/STATICMEMBER_128.cpp
@@ -13,3 +13,14 @@ public:
 	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
 
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printA11() {
+	cout << "ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
