8e00138f71dc4cafe398461cbd6bc832aae5e7ab
iqwa1107 <ramahs49@gmail.com>
2023-05-15 22:43:15 +0700

    membuat void dan int main

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -30,3 +30,23 @@ mahasiswa::mahasiswa(string iNama) {
 mahasiswa::mahasiswa(int iNim, string iNama) {
 	nim = iNim;
 	nama = iNama;
+}
+
+void mahasiswa::cetak() {
+	cout << endl << "Nim = " << nim << endl;
+	cout << "Nama = " << nama << endl;
+}
+
+int main() {
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("indra");
+	mahasiswa mhs4(30, "Fauzan");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+}
